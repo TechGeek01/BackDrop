893a3f21cf76f10985c8cbd7813c6e29a1376c2d
Andy Yasger <techgeek01help@gmail.com>
2020-11-02 16:45:27 -0600

    Removed Unraid reference from window title

--- backdrop.py
+++ backdrop.py
@@ -1452,7 +1452,7 @@ def resource_path(relative_path):
 
 root = tk.Tk()
 root.attributes('-alpha', 0.0)
-root.title('BackDrop - Unraid Drive Backup Tool')
+root.title('BackDrop - Network Drive Backup Tool')
 root.iconbitmap(resource_path('media\\icon.ico'))
 root.resizable(False, False)
 root.geometry('1300x700')
