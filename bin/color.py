934025cf9e3c04d87d7c22251789ed4f5c255599
Andy Yasger <techgeek01help@gmail.com>
2021-01-04 23:56:30 -0600

    Added bcolor class to color.py

--- bin/color.py
+++ bin/color.py
@@ -57,3 +57,14 @@ class Color:
             bool: Whether the pallete is set to dark mode.
         """
         return self.darkMode
+
+class bcolor:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
