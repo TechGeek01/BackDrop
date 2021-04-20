f6aca804c4887bbd5938285d3a7859c0f3e9c97b
Andy Yasger <techgeek01help@gmail.com>
2021-04-20 00:19:21 -0500

    Fix bind_all() causing canvas to scroll when not hovered over

--- bin/widgets.py
+++ bin/widgets.py
@@ -27,7 +27,7 @@ class ScrollableFrame(tk.Frame):
         self.frame.bind('<Configure>', lambda e: self.canvas.configure(
             scrollregion=self.canvas.bbox('all')
         ))
-        self.canvas.bind_all('<MouseWheel>', self._on_mousewheel)
+
         self.canvas.create_window((0, 0), window=self.frame, anchor='nw')
         self.canvas.configure(yscrollcommand=self.vsb.set)
 
@@ -35,8 +35,18 @@ class ScrollableFrame(tk.Frame):
         if scrollbar is None:
             self.vsb.pack(side='left', fill='y')
 
+        self.canvas.bind('<Enter>', self._bind_on_enter)
+        self.canvas.bind('<Leave>', self._unbind_on_leave)
+
     def _on_mousewheel(self, event):
         self.canvas.yview_scroll(int(-1 * event.delta / 120), 'units')
+
+    def _bind_on_enter(self, event):
+        self.canvas.bind_all('<MouseWheel>', self._on_mousewheel)
+
+    def _unbind_on_leave(self, event):
+        # HACK: ScrollableFrame unbind_all will cause problems if mousewheel is bound to anything else
+        self.unbind_all('<MouseWheel>')
 
     def configure(self, *args, **kwargs):
         self.frame.configure(*args, **kwargs)
